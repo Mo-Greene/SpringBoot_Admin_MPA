2f53e3b8dd9748783d90047d9b1741a6d5719404
Mo-Greene <13blueboy13@daum.net>
2023-04-12 17:40:12 +0900

    feat: runtime 예외처리 핸들러

---
+++ src/main/java/com/mogreene/adminmpa/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.mogreene.adminmpa.common.exception;
+
+import com.mogreene.adminmpa.common.api.ApiResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * 예외처리 핸들러
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * RuntimeExceptionHandler
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<ApiResponseDTO<?>> handleRuntimeException(RuntimeException e) {
+
+        log.error(e.getMessage());
+
+        ApiResponseDTO<?> apiResponseDTO = ApiResponseDTO.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .data(e.getMessage())
+                .build();
+
+        return new ResponseEntity<>(apiResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
