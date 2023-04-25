00c3cb00779f7c66d5454c218bd1acb70ad87d76
Mo-Greene <13blueboy13@daum.net>
2023-04-25 19:27:57 +0900

    feat: pagination active

--- src/main/java/com/mogreene/adminmpa/common/interceptor/AdminCheckInterceptor.java
+++ src/main/java/com/mogreene/adminmpa/common/interceptor/AdminCheckInterceptor.java
@@ -32,18 +32,19 @@ public class AdminCheckInterceptor implements HandlerInterceptor {
         String admin = (String) session.getAttribute("admin");
         String redirectUrl = request.getRequestURI().substring(1);
 
+        if (admin == null && !redirectUrl.equals("logout")) {
+            //세션으로 리다이렉트 보내주기
+            session.setAttribute("redirect", redirectUrl);
+            response.sendRedirect("/login");
+            return false;
+        }
+
         //자동로그인 쿠키검증
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals("admin")) {
                 return true;
             }
-        }
-        if (admin == null && !redirectUrl.equals("logout")) {
-            //세션으로 리다이렉트 보내주기
-            session.setAttribute("redirect", redirectUrl);
-            response.sendRedirect("/login");
-            return false;
         }
 
         return true;
